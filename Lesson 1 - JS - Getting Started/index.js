0c30eac916b65d0110130f7b352765691cda0ac8
Rafał Kamiński <rafal.p.kaminski@gmail.com>
2022-09-13 20:52:45 +0200

    Lesson 1 - Commit

---
+++ Lesson 1 - JS - Getting Started/index.js
@@ -0,0 +1,24 @@
+let userName = "Rafał";
+var surname = "Kamiński";
+console.log("To działa");
+const userage = 32;
+userage = 22;
+userName = "Paweł";
+
+let text = "Texty";
+
+let condTrue = true; // 1 swiatło włączone
+let condFalse = false; // 0
+
+let a = "Rafał";
+let empty;
+let empty2 = null;
+let book = {
+  ibn: "1234124123",
+  author: "A.N Ktoś",
+  title: "raz dwa trzy ",
+};
+console.log("To działa");
+let arrayOne = [1, 2, 3, "Tekst", true, book];
+
+console.log("To działa");
