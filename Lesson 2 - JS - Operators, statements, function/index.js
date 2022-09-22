71d1e0512c136b49f8ec2303840dd2d4acd860cf
Rafał Kamiński <rafal.p.kaminski@gmail.com>
2022-09-22 13:52:14 +0200

    Lesson 2 - Adding categories & rangeFn

---
+++ Lesson 2 - JS - Operators, statements, function/index.js
@@ -0,0 +1,63 @@
+// Operatory Artmetyczne - START
+
+// Dodawanie
+
+// Mnożenie
+
+// Dzielenie
+
+// Modulo
+
+// Potęgowanie
+
+// Inkrementacja
+
+// Dekrementacja
+
+// Operatory Artmetyczne - END
+
+// Operatory porównania - START
+
+// Operatory porównania - END
+
+// Operatory Logiczne - START
+
+// Operatory Logiczne - END
+
+// Instrukcje warunkowe - START
+
+// IF
+
+// IF ELSE
+
+// ELSE IF
+
+// SWITCH
+
+// Instrukcje warunkowe - END
+
+// Funkcje - START
+
+// Funkcje - END
+
+// Zasięg zmiennych - START
+
+// function rangeFn(){
+//     {
+//         var x = 2;
+//         let y = 3;
+//         const z = 4;
+//     }
+//     if (true) {
+//         console.log(x);
+//         console.log(y);
+//         console.log(z);
+//     }
+//     console.log(x);
+//     console.log(y);
+//     console.log(z);
+// }
+
+// rangeFn();
+
+// Zasięg zmiennych - END
